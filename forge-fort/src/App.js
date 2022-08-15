d4ec8b4e7dce806a7b1ba5fe9376d9763814f542
TheLonerCoder <thecodingloner@gmail.com>
2022-08-15 09:48:06 -0500

    navbar boiler

--- forge-fort/src/App.js
+++ forge-fort/src/App.js
@@ -1,10 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { Link, Route, Routes } from 'react-router-dom';
+import Home from './components/home';
+import Contact from './components/contact';
+import Explore from './components/explore';
 
 function App() {
   return (
     <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+            <li>
+              <Link to="/explore">Explore</Link>
+            </li>
+          </ul>
+        </nav>
 
+
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/contact' element={<Contact />}/>
+          <Route path='/explore' element={<Explore />}/>
+        </Routes>
     </div>
   );
 }
