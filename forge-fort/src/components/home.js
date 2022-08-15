b0eb59e54a46d60642255823f110aa20508bdef7
TheLonerCoder <thecodingloner@gmail.com>
2022-08-15 13:31:32 -0500

    Update home.js
    Homepage boiler.. just have to update it with information

--- forge-fort/src/components/home.js
+++ forge-fort/src/components/home.js
@@ -1,8 +1,52 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+
+
+
+
+const InfoFlex = styled.div `
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+`
+
+
+const InfoDivs = styled.div `
+ 
+
+  height: 300px;
+  width: 200px;
+  background-color: blue;
+  margin-right: 100px;
+  
+
+`
+
+
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <div id='homeCara' style={{backgroundColor: 'red', width: '100vw', height: '40vh', marginTop: '50px', marginBottom: '50px'}}>
+        <img src="" alt="" />
+      </div>
+    
+      <InfoFlex>
+        <InfoDivs>
+            test
+        </InfoDivs>
+        
+        <InfoDivs>
+            test 2
+        </InfoDivs>
+
+        <InfoDivs>
+            test 3
+        </InfoDivs>
+      </InfoFlex>
+    
+    </div>
   )
 }
 
