af8d06611e831b5da16846bac6c546e10d221940
TheLonerCoder <thecodingloner@gmail.com>
2022-08-24 21:26:11 -0500

    updates

--- forge-fort/src/data/mapdata.js
+++ forge-fort/src/data/mapdata.js
@@ -7,6 +7,7 @@ export const mapObject = {
         id: 'OH',
         name: 'Ohio',
         flag: 'ForgeTheFort2.0/imgs/Flags/oh-flag.png',
+        population: ' 11,799,448',
         cities: {
             largest: [
                 "Columbus (921,605)",
@@ -22,7 +23,7 @@ export const mapObject = {
 ],
             upcoming:{
                 // API call to get article
-                    cityname: '',
+                    cityname: 'Cincinnati',
                     description: '',
                     pictures: []
                 
@@ -31,8 +32,8 @@ export const mapObject = {
         investment: '',
         gdp: {
             // charts
-            total: '',
-            capita: '',
+            total: '736,449.8',
+            capita: '62,517',
         },
         industry: {
             // can use icons to represent industries. include where I got my info from too
@@ -51,22 +52,24 @@ export const mapObject = {
         id: 'IN',
         name: 'Indiana',
         flag: 'ForgeTheFort2.0/imgs/Flags/in-flag.png',
+        population: '6,785,528',
         cities: {
             largest: [
-                "Columbus (921,605)",
-                "Cleveland (374,394)",
-                "Cincinnati (308,929)",
-                "Toledo (266,524)",
-                "Akron (196,736)",
-                "Dayton (140,311)",
-                "Parma (76,546)",
-                "Canton (69,532)",
-                "Youngstown (67,296)",
-                "Lorain (63,468)"
+                "Indianapolis (892,656)",
+                "Fort Wayne (279,228)",
+                "Evansville (117,736)",
+                "Carmel (106,843)",
+                "South Bend (102,191)",
+                "Fishers (101,019)",
+                "Bloomington (87,603)",
+                "Hammond (74,457)",
+                "Gary (73,886)",
+                "Lafayette (70,185)"
+            
 ],
             upcoming:{
                 // API call to get article
-                    cityname: '',
+                    cityname: 'Fort Wayne',
                     description: '',
                     pictures: []
                 
@@ -75,8 +78,8 @@ export const mapObject = {
         investment: '',
         gdp: {
             // charts
-            total: '',
-            capita: '',
+            total: '420,339.2',
+            capita: '61,760',
         },
         industry: {
             // can use icons to represent industries. include where I got my info from too
@@ -95,9 +98,21 @@ export const mapObject = {
         id: 'IL',
         name: 'Illinois',
         flag: 'ForgeTheFort2.0/imgs/Flags/il-flag.png',
+        population: '12,812,508',
         cities: {
             largest: [
-                
+
+                "Chicago (2,671,640)",
+                "Aurora (193,431)",
+                "Naperville (149,526)",
+                "Joliet (146,186)",
+                "Rockford (143,515)",
+                "Springfield (112,400)",
+                "Elgin (108,926)",
+                "Peoria (107,321)",
+                "Champaign (91,654)",
+                "Waukegan (84,260)"
+            
 ],
             upcoming:{
                 // API call to get article
@@ -110,8 +125,8 @@ export const mapObject = {
         investment: '',
         gdp: {
             // charts
-            total: '',
-            capita: '',
+            total: '938,347.3',
+            capita: '74,052',
         },
         industry: {
             // can use icons to represent industries. include where I got my info from too
@@ -130,9 +145,21 @@ export const mapObject = {
         id: 'MI',
         name: 'Michigan',
         flag: 'ForgeTheFort2.0/imgs/Flags/mi-flag.png',
+        population: '',
         cities: {
             largest: [
-                
+
+                "Detroit (661,193)",
+                "Grand Rapids (203,644)",
+                "Warren (132,644)",
+                "Sterling Heights (131,577)",
+                "Lansing (119,047)",
+                "Ann Arbor (115,633)",
+                "Flint (94,374)",
+                "Livonia (92,603)",
+                "Dearborn (92,591)",
+                "Troy (84,035)"
+            
 ],
             upcoming:{
                 // API call to get article
@@ -145,8 +172,8 @@ export const mapObject = {
         investment: '',
         gdp: {
             // charts
-            total: '',
-            capita: '',
+            total: '568,413.2',
+            capita: '56,554 ',
         },
         industry: {
             // can use icons to represent industries. include where I got my info from too
@@ -165,9 +192,21 @@ export const mapObject = {
         id: 'MN',
         name: 'Minnesota',
         flag: 'ForgeTheFort2.0/imgs/Flags/mn-flag.png',
+        population: '',
         cities: {
             largest: [
-                
+
+                "Minneapolis (443,715)",
+                "St. Paul (311,504)",
+                "Rochester (124,599)",
+                "Duluth (84,904)",
+                "Bloomington (83,350)",
+                "Plymouth (81,037)",
+                "Brooklyn Park (79,984)",
+                "Woodbury (77,619)",
+                "Maple Grove (75,355)",
+                "Lakeville (71,934)"
+            
 ],
             upcoming:{
                 // API call to get article
@@ -180,8 +219,8 @@ export const mapObject = {
         investment: '',
         gdp: {
             // charts
-            total: '',
-            capita: '',
+            total: '412,000.8',
+            capita: '72,187',
         },
         industry: {
             // can use icons to represent industries. include where I got my info from too
@@ -200,9 +239,21 @@ export const mapObject = {
         id: 'ND',
         name: 'North Dakota',
         flag: 'ForgeTheFort2.0/imgs/Flags/nd-flag.png',
+        population: '',
         cities: {
             largest: [
-                
+
+                "Fargo (126,375)",
+                "Bismarck (74,429)",
+                "Grand Forks (53,445)",
+                "Minot (47,163)",
+                "West Fargo (39,452)",
+                "Williston (33,767)",
+                "Dickinson (24,444)",
+                "Mandan (23,409)",
+                "Jamestown (14,718)",
+                "Watford City (10,100)"
+            
 ],
             upcoming:{
                 // API call to get article
@@ -215,8 +266,8 @@ export const mapObject = {
         investment: '',
         gdp: {
             // charts
-            total: '',
-            capita: '',
+            total: '63,386.8',
+            capita: '81,795',
         },
         industry: {
             // can use icons to represent industries. include where I got my info from too
@@ -235,9 +286,21 @@ export const mapObject = {
         id: 'SD',
         name: 'South Dakota',
         flag: 'ForgeTheFort2.0/imgs/Flags/sd-flag.png',
+        population: '',
         cities: {
             largest: [
-                
+
+                "Sioux Falls (193,978)",
+                "Rapid City (81,502)",
+                "Aberdeen (27,660)",
+                "Brookings (24,442)",
+                "Watertown (22,156)",
+                "Mitchell (15,637)",
+                "Yankton (14,852)",
+                "Pierre (13,468)",
+                "Huron (13,422)",
+                "Spearfish (12,023)"
+            
 ],
             upcoming:{
                 // API call to get article
@@ -250,8 +313,8 @@ export const mapObject = {
         investment: '',
         gdp: {
             // charts
-            total: '',
-            capita: '',
+            total: '61,205.5 ',
+            capita: '68,357',
         },
         industry: {
             // can use icons to represent industries. include where I got my info from too
@@ -270,9 +333,21 @@ export const mapObject = {
         id: 'MO',
         name: 'Missouri',
         flag: 'ForgeTheFort2.0/imgs/Flags/mo-flag.png',
+        population: '',
         cities: {
             largest: [
-                
+
+                "Kansas City (505,272)",
+                "St. Louis (292,047)",
+                "Springfield (168,911)",
+                "Columbia (125,181)",
+                "Independence (115,637)",
+                "Lee's Summit (101,862)",
+                "O'Fallon (89,600)",
+                "St. Joseph (72,688)",
+                "St. Charles (71,691)",
+                "St. Peters (61,479)"
+            
 ],
             upcoming:{
                 // API call to get article
@@ -285,8 +360,8 @@ export const mapObject = {
         investment: '',
         gdp: {
             // charts
-            total: '',
-            capita: '',
+            total: '359,952.0',
+            capita: '58,356',
         },
         industry: {
             // can use icons to represent industries. include where I got my info from too
@@ -305,9 +380,21 @@ export const mapObject = {
         id: 'KS',
         name: 'Kansas ',
         flag: 'ForgeTheFort2.0/imgs/Flags/ks-flag.png',
+        population: '',
         cities: {
             largest: [
-                
+
+                "Wichita (392,059)",
+                "Overland Park (203,804)",
+                "Kansas City (152,945)",
+                "Olathe (142,906)",
+                "Topeka (123,906)",
+                "Lawrence (101,211)",
+                "Shawnee (65,429)",
+                "Lenexa (56,453)",
+                "Manhattan (53,467)",
+                "Salina (46,079)"
+            
 ],
             upcoming:{
                 // API call to get article
@@ -320,8 +407,8 @@ export const mapObject = {
         investment: '',
         gdp: {
             // charts
-            total: '',
-            capita: '',
+            total: '192,303.6',
+            capita: '65,530',
         },
         industry: {
             // can use icons to represent industries. include where I got my info from too
@@ -340,9 +427,21 @@ export const mapObject = {
         id: 'IA',
         name: 'Iowa ',
         flag: 'ForgeTheFort2.0/imgs/Flags/ia-flag.png',
+        population: '',
         cities: {
             largest: [
-                
+
+                "Des Moines (208,966)",
+                "Cedar Rapids (134,621)",
+                "Davenport (100,606)",
+                "Sioux City (83,464)",
+                "Ankeny (73,676)",
+                "Iowa City (73,432)",
+                "West Des Moines (71,781)",
+                "Ames (67,029)",
+                "Waterloo (66,146)",
+                "Council Bluffs (62,001)"
+            
 ],
             upcoming:{
                 // API call to get article
@@ -355,8 +454,8 @@ export const mapObject = {
         investment: '',
         gdp: {
             // charts
-            total: '',
-            capita: '',
+            total: '219,841.7',
+            capita: '68,849',
         },
         industry: {
             // can use icons to represent industries. include where I got my info from too
@@ -375,9 +474,21 @@ export const mapObject = {
         id: 'NE',
         name: 'Nebraska',
         flag: 'ForgeTheFort2.0/imgs/Flags/ne-flag.png',
+        population: '',
         cities: {
             largest: [
-                
+
+                "Omaha (480,871)",
+                "Lincoln (295,618)",
+                "Bellevue (53,694)",
+                "Grand Island (51,411)",
+                "Kearney (34,683)",
+                "Fremont (26,119)",
+                "Hastings (24,296)",
+                "Norfolk (24,260)",
+                "Columbus (23,954)",
+                "North Platte (23,135)"
+            
 ],
             upcoming:{
                 // API call to get article
@@ -390,8 +501,8 @@ export const mapObject = {
         investment: '',
         gdp: {
             // charts
-            total: '',
-            capita: '',
+            total: '150,387.7',
+            capita: '76,584',
         },
         industry: {
             // can use icons to represent industries. include where I got my info from too
@@ -410,9 +521,21 @@ export const mapObject = {
         id: 'WI',
         name: 'Wisconsin',
         flag: 'ForgeTheFort2.0/imgs/Flags/wi-flag.png',
+        population: '',
         cities: {
             largest: [
-                
+
+                "Milwaukee (586,503)",
+                'Madison (265,158)',
+                "Green Bay (103,813)",
+                "Kenosha (100,052)",
+                "Racine (75,815)",
+                "Appleton (73,258)",
+                "Waukesha (71,837)",
+                "Eau Claire (68,898)",
+                "Oshkosh (67,610)",
+                "Janesville (64,851)"
+            
 ],
             upcoming:{
                 // API call to get article
@@ -425,8 +548,8 @@ export const mapObject = {
         investment: '',
         gdp: {
             // charts
-            total: '',
-            capita: '',
+            total: '365,931.3',
+            capita: '62,065',
         },
         industry: {
             // can use icons to represent industries. include where I got my info from too
