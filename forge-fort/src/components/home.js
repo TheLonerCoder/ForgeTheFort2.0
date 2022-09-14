eb5f5423f3be4dace044765236dfe63f77502c7b
TheLonerCoder <thecodingloner@gmail.com>
2022-09-14 14:14:46 -0500

    Update home.js
    implemented lazy loading on images; they now blur before being loaded

--- forge-fort/src/components/home.js
+++ forge-fort/src/components/home.js
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom';
 import Testchart from '../charts/testchart';
 import { Doughnut } from 'react-chartjs-2';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 
 
 const images = {
@@ -14,6 +15,15 @@ const images = {
   minWidth: "100px",
   maxWidth: "1250px",
   margin: 'margin: 20px 20px 50px 20px'
+
+}
+
+const infoImages = {
+  alt: 'Image load failed',
+  width: "100%",
+  minWidth: "100px",
+  maxWidth: "1250px",
+  // margin: 'margin: 20px 20px 50px 20px'
 
 }
 
@@ -28,6 +38,25 @@ const MyImage = ( image ) => {
         src={image} // use normal <img> attributes as props
         width={images.width} 
         style={images}
+        placeholderSrc={image}
+        />
+      {/* <span>{image.caption}</span> */}
+    </div>
+
+  )
+  };
+
+const LazyImages = ( image ) => {
+  return (
+    <div>
+      <LazyLoadImage
+        alt={infoImages.alt}
+        effect="blur"
+        height={infoImages.height}
+        src={image} // use normal <img> attributes as props
+        width={infoImages.width} 
+        style={infoImages}
+        placeholderSrc={image}
         />
       {/* <span>{image.caption}</span> */}
     </div>
@@ -77,20 +106,24 @@ function Home() {
     
       <div id='infoFlex'>
         <div className='infoDiv'>
-          <InfoImages src="ForgeTheFort2.0/imgs/Unknown/education.jpg" alt='education'/>
+          {/* <InfoImages src="ForgeTheFort2.0/imgs/Unknown/education.jpg" alt='education'/> */}
+          {LazyImages('ForgeTheFort2.0/imgs/Unknown/education.jpg')}
           <h2>Education</h2>
           <p>Here, at <i>Forge the Fort </i>, we believe education is a key fundamental into creating thriving economies and entrepreneurial ecosystems. We've partnered with other institutions and organizations to bring you the best education for your specific needs.</p>
             
         </div>
 
         <div className='infoDiv' id='div2'>
-            <InfoImages src="ForgeTheFort2.0/imgs/Unknown/comm3.jpg" alt="community" />
+            {/* <InfoImages src="ForgeTheFort2.0/imgs/Unknown/comm3.jpg" alt="community" /> */}
+          {LazyImages('ForgeTheFort2.0/imgs/Unknown/comm3.jpg')}
+
           <h2>Community</h2>
           <p>To make sure people can truly thrive, you have sure they have their basic needs met. This includes - but is not limited to - housing, transportation, health, food availability. Every community has their own specific needs and problems so we provide a platform for people to voice those needs and for people within the community, or outside, to come in and solve them. </p>
         </div>
 
         <div className='infoDiv' id='div3'>
-          <InfoImages src="ForgeTheFort2.0/imgs/Unknown/leisure3.jpg" alt="leisure"/>
+          {/* <InfoImages src="ForgeTheFort2.0/imgs/Unknown/leisure3.jpg" alt="leisure"/> */}
+          {LazyImages('ForgeTheFort2.0/imgs/Unknown/leisure3.jpg')}
           <h2>Leisure</h2>
           <p>At the end of the day, one of the best experiences in life is pleasure. We stand by pleasure and providing leisure activities for those within the Midwest, for all seasons, all times of the year. Seeing what works in other communities and what general ideas people have, you can bring these activities to your own.</p>
         </div>
